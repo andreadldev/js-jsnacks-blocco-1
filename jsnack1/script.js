a11b1e2c1b50295d9e2c044abb633193fb006e45
andreadldev <andrea.delisi2@gmail.com>
2022-10-26 11:29:15 +0200

    Correzione

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -2,7 +2,13 @@ const firstWord = prompt("Inserisci parola 1");
 
 const secondWord = prompt("Inserisci parola 2");
 
-if (firstWord.length > secondWord.length) {
-    console.log(`parola più corta: ${secondWord} - parola più lunga: ${firstWord}`);
-} else 
-    console.log(`parola più corta: ${firstWord} - parola più lunga: ${secondWord}`);
+if 
+    (firstWord.length > secondWord.length) {
+    console.log(`parola più corta: ${secondWord} - parola più lunga: ${firstWord}`);}
+
+else if 
+    (firstWord.length < secondWord.length)
+    console.log(`parola più corta: ${firstWord} - parola più lunga: ${secondWord}`)
+
+else 
+    console.log("Le parole hanno la stessa lunghezza");
