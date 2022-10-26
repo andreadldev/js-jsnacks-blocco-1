6f1476e98b86162474a86460530fbeffe3f97fb4
andreadldev <andrea.delisi2@gmail.com>
2022-10-26 11:06:18 +0200

    Rimosso console.log che non serve

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -1,8 +1,6 @@
 const firstWord = prompt("Inserisci parola 1");
-console.log(`${firstWord}`);
 
 const secondWord = prompt("Inserisci parola 2");
-console.log(`${secondWord}`);
 
 if (firstWord.length > secondWord.length) {
     console.log(`parola più corta: ${secondWord} - parola più lunga: ${firstWord}`);
