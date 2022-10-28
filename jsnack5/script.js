431f609868d2291951c2f913a979608411ed77c5
andreadldev <andrea.delisi2@gmail.com>
2022-10-28 10:32:21 +0200

    snack5

---
+++ jsnack5/script.js
@@ -0,0 +1,13 @@
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const oddNumbers = [];
+let sum = 0;
+
+for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] % 2 !== 0) {
+        oddNumbers.push(numbers[i]),
+        sum += numbers[i];
+    }
+}
+
+console.log(`Numeri dispari: ${oddNumbers}`);
+console.log(`Somma numeri dispari: ${sum}`);
